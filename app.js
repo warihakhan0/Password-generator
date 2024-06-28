7d07cfdf8acb18d91f4c5043058b74c1052a50ae
Warihakhan0 <futuretechai0@gmail.com>
2024-06-28 15:38:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerSet = "abcdefghijklmnopqrstuvwxyz";
+const numberSet = "1234567890";
+const symbolSet = "~!@#$%^&*()_+/";
+
+const getRandomData = (dataset) => {
+    return dataset[Math.floor(Math.random()*dataset.length)]
+}
